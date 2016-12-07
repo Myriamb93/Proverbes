519c8f4a8204a0c0263a084dec109335f29770d5
Samuel Szoniecky <samszon@gmail.com>
2016-12-07 11:50:58 +0100

    motif event

--- monde.php
+++ monde.php
@@ -8,8 +8,9 @@
       <script src="js/datamaps.world.js"></script>
       <div id="container" style="position: relative; width: 800px; height: 500px;"></div>
       <script>
-      	var dataMap = [];
-          var map = new Datamap({
+      	var data = [], mousedownID = -1;  //Global ID of mouse down interval
+      	var color = d3.scale.linear().domain([0,10]).range(["green","red"]);
+        var map = new Datamap({
                                  element: document.getElementById('container'), 
                                  fills: { defaultFill: 'rgba(95,0,45,0.9)'
                                         },
@@ -21,21 +22,53 @@
                                   {
                                     datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) 
                                     {
-                                        var m = {};                                        
-                                        //alert(geography.properties.name);
-                                        if(dataMap[geography.id]){
-                                        	if(dataMap[geography.id] == 'red') m[geography.id] = 'green';
-                                        	else m[geography.id] = 'red';
-                                        }else{
-                                            m[geography.id] = 'green';
-                                        }
-                                        dataMap[geography.id] = m;
-                                      	datamap.updateChoropleth(dataMap[geography.id]);
                                      
                                     });
+                                    datamap.svg.selectAll('.datamaps-subunit').on('mousedown', function(geography) 
+                                    {
+                                    	mouseDown(geography);                                                                       
+                                    });
+                                    datamap.svg.selectAll('.datamaps-subunit').on('mouseup', function(geography)
+                                   {
+                                    	mouseUp();
+                                   });
+                                    datamap.svg.selectAll('.datamaps-subunit').on('mouseout', function(geography)
+                                            {
+                                    		mouseUp();
+                                    	});
                                   }
             
                                 });
+
+		//merci à http://stackoverflow.com/questions/15505272/javascript-while-mousedown
+		function mouseDown(geography){
+			console.log('mousedown '+mousedownID);
+            
+            if(mousedownID==-1)  //Prevent multimple loops!
+                mousedownID = setInterval(whilemousedown(geography), 100 /*execute every 100ms*/);
+		}
+		function mouseUp(){
+			console.log('mouseup '+mousedownID);
+            if(mousedownID!=-1) {  //Only stop if exists
+                clearInterval(mousedownID);
+                mousedownID=-1;
+              }                                        
+			
+		}
+        function whilemousedown(geography) {
+
+			console.log('whilemousedown '+mousedownID+' '+geography);
+            if(data[geography.id]){
+            	data[geography.id] ++;
+            }else{
+            	data[geography.id] = 1;
+            }
+            var m = {};                                        
+            m[geography.id] = color(data[geography.id]);
+            map.updateChoropleth(m);
+
+        }
+        
       </script>
 
 
