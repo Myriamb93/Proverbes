a20fea4fd167bd7b07098be2bc78d4becd708260
Myriam <meryem.bennsir@gmail.com>
2016-12-10 14:02:38 +0100

    Revert "onhovernotworking"
    This reverts commit 1718d7a4b71ec5c1348aa28b16e0b1066770468a.

--- js/datamaps.world.js
+++ js/datamaps.world.js
@@ -25,11 +25,8 @@
         borderOpacity: 1,
         borderColor: '#FDFDFD',
         popupTemplate: function(geography, data) {
-          return '<div class="hoverinfo"><strong>' + geography.properties.name   + '</strong></div>';
-          if (timeout)
-          {
-            return '<div class="hoverinfo"><strong>' + geography.properties.name   + 'hello'+ geography.properties.id+ '</strong></div>';
-          }
+          return '<div class="hoverinfo"><strong>' + geography.properties.name + geography.properties.id+'</strong></div>';
+
         },
         popupOnHover: true,
         highlightOnHover: true,
--- monde.php
+++ monde.php
@@ -23,12 +23,12 @@
                                     datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) 
                                     {
                                      
-                                    });                         
-
+                                    });
                                     datamap.svg.selectAll('.datamaps-subunit').on('mousedown', function(geography) 
                                     {
                                     	mouseDown(geography);                                                                       
                                     });
+
                              
                                     datamap.svg.selectAll('.datamaps-subunit').on('mouseup', function(geography)
                                    {
@@ -44,7 +44,6 @@
 
 
  var timeout ;
- var m = {}; 
  
 function mouseDown(geography){
  
@@ -57,11 +56,11 @@ function mouseDown(geography){
             }else{
               data[geography.id] = 1;
             }
-                                                   
+            var m = {};                                        
             m[geography.id] = color(data[geography.id]);
             map.updateChoropleth(m);}, 100);
           
-
+           
     return false;
 }
 
@@ -74,16 +73,9 @@ clearInterval(timeout);
 
 
 
-function dosomething(geography)
-{
 
-  if(data[geography.id]){
 
-    
-  }
-}
 
-  
 
 
 /*
