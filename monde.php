c6d5ddb15545d16c0085dd7051638d8e97f82e91
Myriam <meryem.bennsir@gmail.com>
2016-12-10 09:13:00 +0100

    while mousedow,

--- monde.php
+++ monde.php
@@ -9,7 +9,7 @@
       <div id="container" style="position: relative; width: 800px; height: 500px;"></div>
       <script>
       	var data = [], mousedownID = -1;  //Global ID of mouse down interval
-      	var color = d3.scale.linear().domain([0,10]).range(["green","red"]);
+      	var color = d3.scale.linear().domain([0,100]).range(["green","red"]);
         var map = new Datamap({
                                  element: document.getElementById('container'), 
                                  fills: { defaultFill: 'rgba(95,0,45,0.9)'
@@ -28,6 +28,11 @@
                                     {
                                     	mouseDown(geography);                                                                       
                                     });
+
+                                   /* datamap.svg.selectAll('.datamaps-subunit').on('mouseover', function(geography) 
+                                    {
+                                      mouseOver(geography);                                                                       
+                                    });*/
                                     datamap.svg.selectAll('.datamaps-subunit').on('mouseup', function(geography)
                                    {
                                     	mouseUp();
@@ -40,12 +45,102 @@
             
                                 });
 
+
+ var timeout ;
+ var test= false ;
+function mouseDown(geography){
+ 
+    timeout = setInterval( function(){
+
+
+            if(data[geography.id]){
+
+              data[geography.id] ++;
+            }else{
+              data[geography.id] = 1;
+            }
+            var m = {};                                        
+            m[geography.id] = color(data[geography.id]);
+            map.updateChoropleth(m);}, 100);
+            test= true ; 
+
+    return false;
+}
+
+
+function mouseUp(geography){
+
+clearInterval(timeout);
+    return false;
+}
+
+
+
+/*function mouseOver(geography){
+  if (test)
+
+  return '<div class="hoverinfo"><strong>' + data[geography.id] + '</strong></div>';
+}
+*/
+
+
+
+/*
+ var loopthis;
+function mouseDown (geography) {
+    loopthis = setInterval(repeatingfunction(geography), 100);
+}
+
+
+
+function mouseUp () {
+    clearInterval(loopthis);
+}
+
+function repeatingfunction(geography) {
+
+var m = {};    
+console.log(data[geography.id] );
+
+
+
+    if(data[geography.id])
+    {
+
+        while( data[geography.id] < 100 )
+          {
+           data[geography.id]++;
+           m[geography.id] = color(data[geography.id]);
+            map.updateChoropleth(m);
+          }
+
+          while( data[geography.id] > -1 )
+          {
+              data[geography.id]--;
+              m[geography.id] = color(data[geography.id]);
+            map.updateChoropleth(m);
+          }
+    }
+  else
+  {
+    data[geography.id] = 1;
+    m[geography.id] = color(data[geography.id]);
+            map.updateChoropleth(m);
+  }
+                                                
+            
+}
+
+*/
+
+
+
 		//merci à http://stackoverflow.com/questions/15505272/javascript-while-mousedown
-		function mouseDown(geography){
+	/*	function mouseDown(geography){
 			console.log('mousedown '+mousedownID);
             
             if(mousedownID==-1)  //Prevent multimple loops!
-                mousedownID = setInterval(whilemousedown(geography), 100 /*execute every 100ms*/);
+                mousedownID = setInterval(whilemousedown(geography), 100 );
 		}
 		function mouseUp(){
 			console.log('mouseup '+mousedownID);
@@ -67,7 +162,10 @@
             m[geography.id] = color(data[geography.id]);
             map.updateChoropleth(m);
 
-        }
+            if(mousedownID!=-1) 
+             mousedownID = setInterval(whilemousedown, );
+
+        }*/
         
       </script>
 
